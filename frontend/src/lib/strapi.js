436bac628b2603e2e44f7f6616023061c52acf7e
Nick Roels <nick.roels@student.kdg.be>
2025-12-11 11:16:09 +0100

    Initial commit

---
+++ frontend/src/lib/strapi.js
@@ -0,0 +1,38 @@
+export default async function fetchApi({
+    endpoint,
+    query,
+    wrappedByKey,
+    wrappedByList,
+    page
+}) {
+    if (endpoint.startsWith('/')) {
+        endpoint = endpoint.slice(1);
+    }
+
+    const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+
+    if (query) {
+        Object.entries(query).forEach(([key, value]) => {
+            url.searchParams.append(key, value);
+        });
+    }
+
+    if (page) {
+        url.searchParams.append(`populate[${page}][populate]`, '*');
+    } else {
+        url.searchParams.append('populate', '*');
+    }
+
+    const res = await fetch(url.toString());
+    let data = await res.json();
+
+    if (wrappedByKey) {
+        data = data[wrappedByKey];
+    }
+
+    if (wrappedByList) {
+        data = data[0];
+    }
+
+    return data;
+}
